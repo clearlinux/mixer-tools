f98416365beca183b5990bb2d275b1ed8cb01aab
Tudor Marcu <tudor.marcu@intel.com>
2016-03-24 00:49:28 -0700

    Add options to create-update.sh
    To support more configurations, the script now supports passing a specific
    builder.conf and format name to use when creating the update content.
    
    Signed-off-by: Tudor Marcu <tudor.marcu@intel.com>

--- create-update.sh
+++ create-update.sh
@@ -3,19 +3,50 @@ set -e
 
 CLRVER=$(cat "$PWD/.clear-version")
 MIXVER=$(cat "$PWD/.mix-version")
-STATE_DIR=$(grep STATE_DIR "/usr/share/defaults/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | sed 's/ *//')
-if [ -z "$1" ]; then
-        FORMAT="staging"
+
+
+while [[ $# > 0 ]]
+do
+  key="$1"
+  case $key in
+    -c|--config)
+    BUILDERCONF="$2"
+    shift
+    ;;
+    -f|--format)
+    FORMAT="$2"
+    shift
+    ;;
+    -h|--help)
+    echo -e "Usage: mixer-create-update.sh\n"
+    echo -e "\t-c, --config Supply specific builder.conf\n"
+    echo -e "\t-f, --format Supply format to use\n"
+    exit
+    ;;
+    *)
+    echo -e "Invalid option\n"
+    exit
+    ;;
+esac
+shift
+done
+
+if [ ! -z "$BUILDERCONF" ]; then
+  STATE_DIR=$(grep STATE_DIR "$BUILDERCONF" | cut -d "=" -f2 | sed 's/ *//')
+  BUNDLE_DIR=$(grep BUNDLE_DIR "$BUILDERCONF" | cut -d "=" -f2 | sed 's/ *//')
 else
-        FORMAT=$1
+  STATE_DIR=$(grep STATE_DIR "/usr/share/defaults/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | sed 's/ *//')
+  BUNDLE_DIR=$(grep BUNDLE_DIR "/usr/share/defaults/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | sed 's/ *//')
 fi
-echo "format$FORMAT"
 
-export BUNDLEREPO="$PWD/.repos/clr-bundles"
+if [ -z "$FORMAT" ]; then
+        FORMAT="staging"
+fi
 
-if [ ! -d "$STATE_DIR/www/version" ]; then
-	mkdir -p "$STATE_DIR/www/version/formatstaging/"
-	echo
+export BUNDLEREPO="$BUNDLE_DIR"
+
+if [ ! -d "$STATE_DIR/www/version/format$FORMAT" ]; then
+	sudo -E mkdir -p "$STATE_DIR/www/version/format$FORMAT/"
 fi
 
 # step 1: create update content for current mix
@@ -25,7 +56,7 @@ sudo -E "swupd_create_update" -S "$STATE_DIR" --osversion $MIXVER
 sudo -E "swupd_make_fullfiles" -S "$STATE_DIR" $MIXVER
 
 # step 3: create zero/delta packs
-for bundle in $(ls "$BUNDLEREPO/bundles/"); do
+for bundle in $(ls "$BUNDLEREPO"); do
 	sudo -E "swupd_make_pack" -S "$STATE_DIR" 0 $MIXVER $bundle
 done
 

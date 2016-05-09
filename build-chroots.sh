8d5306e345737e1c5d10be2d7989ba2eb5637d0e
Tudor Marcu <tudor.marcu@intel.com>
2016-05-09 23:37:22 +0000

    Fix formatting and cleanup image/files-* entries
    The files-* entries in the image dir are not needed after buildling chroots
    since they are copied over to the webdir/VER/noship directory, so remove them.
    
    Signed-off-by: Tudor Marcu <tudor.marcu@intel.com>

--- build-chroots.sh
+++ build-chroots.sh
@@ -6,23 +6,23 @@ MIXVER=$(cat "$PWD/.mix-version")
 
 while [[ $# > 0 ]]
 do
-  key="$1"
-  case $key in
-    -c|--config)
-    BUILDERCONF="$2"
+    key="$1"
+    case $key in
+        -c|--config)
+        BUILDERCONF="$2"
+        shift
+        ;;
+        -h|--help)
+        echo -e "Usage: mixer-build-chroots.sh\n"
+        echo -e "\t-c, --config Supply specific builder.conf\n"
+        exit
+        ;;
+        *)
+        echo -e "Invalid option\n"
+        exit
+        ;;
+    esac
     shift
-    ;;
-    -h|--help)
-    echo -e "Usage: mixer-build-chroots.sh\n"
-    echo -e "\t-c, --config Supply specific builder.conf\n"
-    exit
-    ;;
-    *)
-    echo -e "Invalid option\n"
-    exit
-    ;;
-esac
-shift
 done
 
 # FIXME: For now, only build chroots for the mix. In the future, when we run the
@@ -38,11 +38,11 @@ unset http_proxy
 unset https_proxy
 
 check_dep() {
-  type $1 &> /dev/null
-  if [ $? -ne 0 ]; then
-    echo "$1 program not found... Unable to continue"
-    exit 1
-  fi
+    type $1 &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "$1 program not found... Unable to continue"
+        exit 1
+    fi
 }
 
 # Check dependencies
@@ -56,7 +56,7 @@ check_dep "rpm"
 check_dep "yum"
 
 if [ ! -e "$PWD/yum.conf.in" ]; then
-  cp /usr/share/defaults/mixer/yum.conf.in .
+    cp /usr/share/defaults/mixer/yum.conf.in .
 fi
 
 # Strip the trailing and leading whitespace on variables to sanitize them
@@ -66,40 +66,46 @@ function strip_whitespace {
 
 # Read values from builder.conf, either supplied or default
 if [[ ! -z $BUILDERCONF ]]; then
-  STATE_DIR=$(grep STATE_DIR "$BUILDERCONF" | cut -d "=" -f2 | strip_whitespace)
-  YUM_CONF=$(grep YUM_CONF "$BUILDERCONF" | cut -d "=" -f2 | strip_whitespace)
+    STATE_DIR=$(grep STATE_DIR "$BUILDERCONF" | cut -d "=" -f2 | strip_whitespace)
+    YUM_CONF=$(grep YUM_CONF "$BUILDERCONF" | cut -d "=" -f2 | strip_whitespace)
 elif [ -e "/etc/bundle-chroot-builder/builder.conf" ]; then
-  STATE_DIR=$(grep STATE_DIR "/etc/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | strip_whitespace)
-  YUM_CONF=$(grep YUM_CONF "/etc/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | strip_whitespace)
+    STATE_DIR=$(grep STATE_DIR "/etc/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | strip_whitespace)
+    YUM_CONF=$(grep YUM_CONF "/etc/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | strip_whitespace)
 else
-  STATE_DIR=$(grep STATE_DIR "/usr/share/defaults/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | strip_whitespace)
-  YUM_CONF=$(grep YUM_CONF "/usr/share/defaults/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | strip_whitespace)
+    STATE_DIR=$(grep STATE_DIR "/usr/share/defaults/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | strip_whitespace)
+    YUM_CONF=$(grep YUM_CONF "/usr/share/defaults/bundle-chroot-builder/builder.conf" | cut -d "=" -f2 | strip_whitespace)
 fi
 
 if [ "$BUILDTYPE" = "clear" ]; then
-  m4 yum.conf.in > "$PWD/.yum-clear.conf"
-  BUILDVER=$CLRVER
+    m4 yum.conf.in > "$PWD/.yum-clear.conf"
+    BUILDVER=$CLRVER
 elif [ "$BUILDTYPE" = "mix" ]; then
-  if [ ! -f "$PWD/.mixer-repopath" ] ; then
-    m4 yum.conf.in > "$PWD/.yum-mix.conf"
-  else
-    repopath=$(cat "$PWD/.mixer-repopath" | xargs realpath)
-    m4 -D MIXER_REPO -D MIXER_REPOPATH="$repopath" yum.conf.in > "$PWD/.yum-mix.conf"
-  fi
-  BUILDVER=$MIXVER
+    if [ ! -f "$PWD/.mixer-repopath" ] ; then
+        m4 yum.conf.in > "$PWD/.yum-mix.conf"
+    else
+        repopath=$(cat "$PWD/.mixer-repopath" | xargs realpath)
+        m4 -D MIXER_REPO -D MIXER_REPOPATH="$repopath" yum.conf.in > "$PWD/.yum-mix.conf"
+    fi
+    BUILDVER=$MIXVER
 fi
 
 # if BUILDVER already exists wipe it so it's a fresh build
 if [ -d $STATE_DIR/image/$BUILDVER ] ; then
-  echo -e "Wiping away previous version $BUILDVER...\n"
-  sudo -E rm -rf "$STATE_DIR/www/$BUILDVER"
-  sudo -E rm -rf "$STATE_DIR/image/$BUILDVER"
+    echo -e "Wiping away previous version $BUILDVER...\n"
+    sudo -E rm -rf "$STATE_DIR/www/$BUILDVER"
+    sudo -E rm -rf "$STATE_DIR/image/$BUILDVER"
 fi
 
 # if this is a mix, need to build with the Clear version, but publish the mix version
 if [[ ! -z $BUILDERCONF ]]; then
-  sudo -E sh -c "LD_PRELOAD=/usr/lib64/nosync/nosync.so $BUILDERSCRIPT -c $BUILDERCONF -m $BUILDVER $CLRVER"
+    sudo -E sh -c "LD_PRELOAD=/usr/lib64/nosync/nosync.so $BUILDERSCRIPT -c $BUILDERCONF -m $BUILDVER $CLRVER"
 else
-  sudo -E sh -c "LD_PRELOAD=/usr/lib64/nosync/nosync.so $BUILDERSCRIPT -m $BUILDVER $CLRVER"
+    sudo -E sh -c "LD_PRELOAD=/usr/lib64/nosync/nosync.so $BUILDERSCRIPT -m $BUILDVER $CLRVER"
 fi
-# vi: ts=8 sw=2 sts=2 et tw=80
+
+# clean up the files-* entries since they are now copied into the webdir noship
+for i in $(ls $STATE_DIR/image/$MIXVER | grep files-*);
+do
+    sudo rm -f $STATE_DIR/image/$MIXVER/$i;
+done;
+# vi: ts=8 sw=4 sts=4 et tw=80

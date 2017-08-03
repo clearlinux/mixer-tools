4270a98458dac19c2a4ed77cae139a0249fa6487
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2017-08-03 10:36:06 -0700

    Move command implementation to their own functions
    Instead of having the flag specification, flag parsing, and command
    processing in three different parts of the main function, move the
    whole code for each command to its own function.
    
    This patch also fixes the issue that build-all was always using the
    default values of the flags, since the build-update specific flags
    were not being added to its FlagSet. The problem was hard to see
    because all commands shared the same scope.
    
    Signed-off-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

--- src/mixer/main.go
+++ src/mixer/main.go
@@ -11,140 +11,188 @@ import (
 	"helpers"
 )
 
-// PrintMainHelp emits useful help text to the console
+type Command struct {
+	Name        string
+	Description string
+	Run         func(args []string)
+}
+
+var commands []*Command
+
+func init() {
+	commands = []*Command{
+		{"build-all", "Build all content for mix with default options", cmdBuildAll},
+		{"build-chroots", "Build chroots for the mix", cmdBuildChroots},
+		{"build-update", "Build all update content for the mix", cmdBuildUpdate},
+		{"build-image", "Build an image from the mix content", cmdBuildImage},
+		{"add-rpms", "Add rpms to local yum repository", cmdAddRPMs},
+		{"get-bundles", "Get the clr-bundles from upstream", cmdGetBundles},
+		{"init-mix", "Initialize the mixer and workspace", cmdInitMix},
+		{"help", "Show help options", cmdHelp},
+	}
+}
+
 func PrintMainHelp() {
 	fmt.Printf("usage: mixer <command> [args]\n")
-	fmt.Printf("\tbuild-all\t\tBuild all content for mix with default options\n")
-	fmt.Printf("\tbuild-chroots\t\tBuild chroots for the mix\n")
-	fmt.Printf("\tbuild-update\t\tBuild all update content for the mix\n")
-	fmt.Printf("\tbuild-image\t\tBuild an image from the mix content\n")
-	fmt.Printf("\tadd-rpms\t\tAdd rpms to local yum repository\n")
-	fmt.Printf("\tget-bundles\t\tGet the clr-bundles from upstream\n")
-	fmt.Printf("\tinit-mix\t\tInitialize the mixer and workspace\n")
-	fmt.Printf("\thelp     \t\tShow help options\n")
+	for _, cmd := range commands {
+		fmt.Printf("\t%-20s\t%s\n", cmd.Name, cmd.Description)
+	}
 }
 
 func main() {
 	fmt.Println("Mixer 3.06")
 	os.Setenv("LD_PRELOAD", "/usr/lib64/nosync/nosync.so")
 
-	addcmd := flag.NewFlagSet("add-rpms", flag.ExitOnError)
-	addconf := addcmd.String("config", "", "Supply a specific builder.conf to use for mixing")
-
-	buildall := flag.NewFlagSet("build-all", flag.ExitOnError)
-	buildallconf := buildall.String("config", "", "Supply a specific builder.conf to use for mixing")
-
-	chrootcmd := flag.NewFlagSet("build-chroots", flag.ExitOnError)
-	certflag := chrootcmd.Bool("no-signing", false, "Do not generate a certificate to sign the Manifest.MoM")
-	chrootconf := chrootcmd.String("config", "", "Supply a specific builder.conf to use for mixing")
-
-	updatecmd := flag.NewFlagSet("build-update", flag.ExitOnError)
-	updateconf := updatecmd.String("config", "", "Supply a specific builder.conf to use for mixing")
-	formatflag := updatecmd.String("format", "", "Supply format to use")
-	incrementflag := updatecmd.Bool("increment", false, "Automatically increment the mixversion post build")
-	minvflag := updatecmd.Int("minversion", 0, "Supply minversion to build update with")
-	signflag := updatecmd.Bool("no-signing", false, "Do not generate a certificate and do not sign the Manifest.MoM")
-	prefixflag := updatecmd.String("prefix", "", "Supply prefix for where the swupd binaries live")
-	publishflag := updatecmd.Bool("no-publish", false, "Do not update the latest version after update")
-	keepchrootsflag := updatecmd.Bool("keep-chroots", false, "Keep individual chroots created and not just consolidated 'full'")
-
-	bundlescmd := flag.NewFlagSet("get-bundles", flag.ExitOnError)
-	bundleconf := bundlescmd.String("config", "", "Supply a specific builder.conf to use for mixing")
-
-	initcmd := flag.NewFlagSet("init-mix", flag.ExitOnError)
-	allflag := initcmd.Bool("all", false, "Create a mix with all Clear bundles included")
-	clearflag := initcmd.Int("clearver", 0, "Supply the Clear version to compose the mix from")
-	mixflag := initcmd.Int("mixver", 0, "Supply the Mix version to build")
-	initconf := initcmd.String("config", "", "Supply a specific builder.conf to use for mixing")
-
-	imagecmd := flag.NewFlagSet("build-image", flag.ExitOnError)
-	imageformat := imagecmd.String("format", "", "Supply the format used for the Mix")
-
 	if len(os.Args) == 1 {
 		PrintMainHelp()
 		return
 	}
 
-	switch os.Args[1] {
-	case "help", "-h":
-		PrintMainHelp()
-		return
-	case "build-all":
-		buildall.Parse(os.Args[2:])
-	case "build-chroots":
-		chrootcmd.Parse(os.Args[2:])
-	case "build-update":
-		updatecmd.Parse(os.Args[2:])
-	case "build-image":
-		imagecmd.Parse(os.Args[2:])
-	case "add-rpms":
-		addcmd.Parse(os.Args[2:])
-	case "get-bundles":
-		bundlescmd.Parse(os.Args[2:])
-	case "init-mix":
-		initcmd.Parse(os.Args[2:])
-	default:
-		fmt.Printf("%q is not valid command.\n", os.Args[1])
+	var cmd *Command
+	name := os.Args[1]
+	if name == "-h" {
+		name = "help"
+	}
+
+	for _, c := range commands {
+		if c.Name == name {
+			cmd = c
+		}
+	}
+
+	if cmd == nil {
+		fmt.Printf("%q is not a valid command.\n", name)
 		os.Exit(-1)
 	}
 
-	// If we got this far, the flags are correct, so read the conf from
-	// the current directory or from the flag passed in
-	if addcmd.Parsed() {
-		b := builder.NewFromConfig(*addconf)
-		rpms, err := ioutil.ReadDir(b.Rpmdir)
-		if err != nil {
-			fmt.Printf("ERROR: cannot read %s\n", b.Rpmdir)
-		}
+	args := os.Args[2:]
+	cmd.Run(args)
+}
+
+type UpdateVars struct {
+	Format     string
+	Increment  bool
+	MinVersion int
+	NoSigning  bool
+	Prefix     string
+	NoPublish  bool
+	KeepChroot bool
+}
+
+func setupUpdateFlags(v *UpdateVars, fs *flag.FlagSet) {
+	fs.StringVar(&v.Format, "format", "", "Supply format to use")
+	fs.BoolVar(&v.Increment, "increment", false, "Automatically increment the mixversion post build")
+	fs.IntVar(&v.MinVersion, "minversion", 0, "Supply minversion to build update with")
+	fs.BoolVar(&v.NoSigning, "no-signing", false, "Do not generate a certificate and do not sign the Manifest.MoM")
+	fs.StringVar(&v.Prefix, "prefix", "", "Supply prefix for where the swupd binaries live")
+	fs.BoolVar(&v.NoPublish, "no-publish", false, "Do not update the latest version after update")
+	fs.BoolVar(&v.KeepChroot, "keep-chroots", false, "Keep individual chroots created and not just consolidated 'full'")
+}
+
+func cmdBuildAll(args []string) {
+	fs := flag.NewFlagSet("build-all", flag.ExitOnError)
+	config := fs.String("config", "", "Supply a specific builder.conf to use for mixing")
+
+	v := &UpdateVars{}
+	setupUpdateFlags(v, fs)
+
+	fs.Parse(args)
+
+	b := builder.NewFromConfig(*config)
+	rpms, err := ioutil.ReadDir(b.Rpmdir)
+	if err == nil {
 		b.AddRPMList(rpms)
 	}
+	BuildChroots(b, v.NoSigning)
+	err = b.BuildUpdate(v.Prefix, v.MinVersion, v.Format, v.NoSigning, !v.NoPublish, v.KeepChroot)
+	if err != nil {
+		os.Exit(-1)
+	}
 
-	if buildall.Parsed() {
-		b := builder.NewFromConfig(*buildallconf)
-		rpms, err := ioutil.ReadDir(b.Rpmdir)
-		if err == nil {
-			b.AddRPMList(rpms)
-		}
-		BuildChroots(b, *certflag)
-		BuildUpdate(b, *prefixflag, *minvflag, *formatflag, *signflag, !(*publishflag), *keepchrootsflag)
+	b.UpdateMixVer()
+}
+
+func cmdBuildChroots(args []string) {
+	fs := flag.NewFlagSet("build-chroots", flag.ExitOnError)
+	config := fs.String("config", "", "Supply a specific builder.conf to use for mixing")
+	noSigning := fs.Bool("no-signing", false, "Do not generate a certificate to sign the Manifest.MoM")
+
+	fs.Parse(args)
+
+	b := builder.NewFromConfig(*config)
+	BuildChroots(b, *noSigning)
+}
+
+func cmdBuildUpdate(args []string) {
+	fs := flag.NewFlagSet("build-update", flag.ExitOnError)
+	config := fs.String("config", "", "Supply a specific builder.conf to use for mixing")
+
+	v := &UpdateVars{}
+	setupUpdateFlags(v, fs)
+
+	fs.Parse(args)
+
+	b := builder.NewFromConfig(*config)
+	err := b.BuildUpdate(v.Prefix, v.MinVersion, v.Format, v.NoSigning, !v.NoPublish, v.KeepChroot)
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	if v.Increment {
 		b.UpdateMixVer()
-	}
-
-	if bundlescmd.Parsed() {
-		b := builder.NewFromConfig(*bundleconf)
-		fmt.Println("Getting clr-bundles for version " + b.Get("Clearver"))
-		b.UpdateRepo(b.Get("Clearver"), false)
-	}
-
-	if initcmd.Parsed() {
-		b := builder.New()
-		b.LoadBuilderConf(*initconf)
-		b.ReadBuilderConf()
-		b.InitMix(strconv.Itoa(*clearflag), strconv.Itoa(*mixflag), *allflag)
-	}
-
-	if chrootcmd.Parsed() {
-		b := builder.NewFromConfig(*chrootconf)
-		BuildChroots(b, *certflag)
-	}
-
-	if updatecmd.Parsed() {
-		b := builder.NewFromConfig(*updateconf)
-		BuildUpdate(b, *prefixflag, *minvflag, *formatflag, *signflag, !(*publishflag), *keepchrootsflag)
-
-		if *incrementflag == true {
-			b.UpdateMixVer()
-		}
-	}
-
-	if imagecmd.Parsed() {
-		b := builder.NewFromConfig("")
-		b.BuildImage(*imageformat)
 	}
 }
 
-func BuildChroots(config interface{}, signflag bool) {
-	builder := config.(*builder.Builder)
+func cmdBuildImage(args []string) {
+	imagecmd := flag.NewFlagSet("build-image", flag.ExitOnError)
+	imageformat := imagecmd.String("format", "", "Supply the format used for the Mix")
+
+	imagecmd.Parse(args)
+
+	b := builder.NewFromConfig("")
+	b.BuildImage(*imageformat)
+}
+
+func cmdAddRPMs(args []string) {
+	flags := flag.NewFlagSet("add-rpms", flag.ExitOnError)
+	conf := flags.String("config", "", "Supply a specific builder.conf to use for mixing")
+	flags.Parse(args)
+
+	b := builder.NewFromConfig(*conf)
+	rpms, err := ioutil.ReadDir(b.Rpmdir)
+	if err != nil {
+		fmt.Printf("ERROR: cannot read %s\n", b.Rpmdir)
+	}
+	b.AddRPMList(rpms)
+}
+
+func cmdGetBundles(args []string) {
+	bundlescmd := flag.NewFlagSet("get-bundles", flag.ExitOnError)
+	bundleconf := bundlescmd.String("config", "", "Supply a specific builder.conf to use for mixing")
+	bundlescmd.Parse(args)
+	b := builder.NewFromConfig(*bundleconf)
+	fmt.Println("Getting clr-bundles for version " + b.Get("Clearver"))
+	b.UpdateRepo(b.Get("Clearver"), false)
+}
+
+func cmdInitMix(args []string) {
+	initcmd := flag.NewFlagSet("init-mix", flag.ExitOnError)
+	allflag := initcmd.Bool("all", false, "Create a mix with all Clear bundles included")
+	clearflag := initcmd.Int("clearver", 0, "Supply the Clear version to compose the mix from")
+	mixflag := initcmd.Int("mixver", 0, "Supply the Mix version to build")
+	initconf := initcmd.String("config", "", "Supply a specific builder.conf to use for mixing")
+	initcmd.Parse(args)
+	b := builder.New()
+	b.LoadBuilderConf(*initconf)
+	b.ReadBuilderConf()
+	b.InitMix(strconv.Itoa(*clearflag), strconv.Itoa(*mixflag), *allflag)
+}
+
+func cmdHelp(args []string) {
+	PrintMainHelp()
+}
+
+func BuildChroots(builder *builder.Builder, signflag bool) {
 	// Create the signing and validation key/cert
 	if _, err := os.Stat(builder.Get("Cert")); os.IsNotExist(err) {
 		fmt.Println("Generating certificate for signature validation...")
@@ -163,13 +211,5 @@ func BuildChroots(config interface{}, signflag bool) {
 		if err != nil {
 			os.Exit(-1)
 		}
-	}
-}
-
-func BuildUpdate(config interface{}, prefixflag string, minvflag int, formatflag string, signflag bool, publishflag bool, keepchrootsflag bool) {
-	builder := config.(*builder.Builder)
-	err := builder.BuildUpdate(prefixflag, minvflag, formatflag, signflag, publishflag, keepchrootsflag)
-	if err != nil {
-		os.Exit(-1)
 	}
 }

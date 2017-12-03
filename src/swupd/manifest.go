de7b7e183b9dbcb1e5bcb3581b364116d5beb6a1
Matthew Johnson <matthew.johnson@intel.com>
2017-12-03 14:54:49 -0800

    swupd: add manifest read functions
    Add manifest readers and their associated tests.
    
    Signed-off-by: Matthew Johnson <matthew.johnson@intel.com>

---
+++ src/swupd/manifest.go
@@ -0,0 +1,226 @@
+package swupd
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const manifestFieldDelim = "\t"
+
+// ManifestHeader contains metadata for the manifest
+type ManifestHeader struct {
+	Format      uint
+	Version     uint32
+	Previous    uint32
+	FileCount   uint32
+	TimeStamp   time.Time
+	ContentSize uint64
+	Includes    []*Manifest
+}
+
+// Manifest represents a bundle or list of bundles (MoM)
+type Manifest struct {
+	Name   string
+	Header ManifestHeader
+	Files  []*File
+}
+
+// MoM is a manifest of manifests with the same header information
+type MoM struct {
+	Header       ManifestHeader
+	SubManifests []*Manifest
+}
+
+// readManifestFileHeaderLine Read a header line from a manifest
+func readManifestFileHeaderLine(fields []string, m *Manifest) error {
+	var err error
+	var parsed uint64
+
+	// Only search for defined fields
+	switch fields[0] {
+	case "MANIFEST":
+		if parsed, err = strconv.ParseUint(fields[1], 10, 16); err != nil {
+			return err
+		}
+		m.Header.Format = uint(parsed)
+	case "version:":
+		if parsed, err = strconv.ParseUint(fields[1], 10, 32); err != nil {
+			return err
+		}
+		m.Header.Version = uint32(parsed)
+	case "previous:":
+		if parsed, err = strconv.ParseUint(fields[1], 10, 32); err != nil {
+			return err
+		}
+		m.Header.Previous = uint32(parsed)
+	case "filecount:":
+		if parsed, err = strconv.ParseUint(fields[1], 10, 32); err != nil {
+			return err
+		}
+		m.Header.FileCount = uint32(parsed)
+	case "timestamp:":
+		var timestamp int64
+		if timestamp, err = strconv.ParseInt(fields[1], 10, 64); err != nil {
+			return err
+		}
+		// parsed is already int64
+		m.Header.TimeStamp = time.Unix(timestamp, 0)
+	case "contentsize:":
+		if parsed, err = strconv.ParseUint(fields[1], 10, 64); err != nil {
+			return err
+		}
+		// parsed is already uint64
+		m.Header.ContentSize = parsed
+	case "includes:":
+		m.Header.Includes = append(m.Header.Includes, &Manifest{Name: fields[1]})
+	}
+
+	return nil
+}
+
+// readManifestFileEntry
+// fields: "<fflags, 4 chars>", "<hash, 64 chars>", "<version>", "<filename>"
+func readManifestFileEntry(fields []string, m *Manifest) error {
+	fflags := fields[0]
+	fhash := fields[1]
+	fver := fields[2]
+	fname := fields[3]
+
+	// check length of fflags and fhash
+	if len(fflags) != 4 {
+		return fmt.Errorf("invalid number of flags: %v", fflags)
+	}
+
+	if len(fhash) != 64 {
+		return fmt.Errorf("invalid hash length: %v", fhash)
+	}
+
+	var parsed uint64
+	var err error
+	// fver must be a valid uint32
+	if parsed, err = strconv.ParseUint(fver, 10, 32); err != nil {
+		return fmt.Errorf("invalid version: %v", err)
+	}
+	ver := uint32(parsed)
+
+	// create a file record
+	var file *File
+	file = &File{Name: fname, Hash: fhash, Version: ver}
+	// Set the flags using fflags
+	if err = file.setFlags(fflags); err != nil {
+		return fmt.Errorf("invalid flags: %v", err)
+	}
+
+	// add file to manifest
+	m.Files = append(m.Files, file)
+
+	return nil
+}
+
+// CheckHeaderPopulated checks that all header fields in the manifest are populated
+func (m *Manifest) CheckHeaderPopulated() error {
+	if m.Header.Format == 0 {
+		return errors.New("manifest format not set")
+	}
+
+	if m.Header.Version == 0 {
+		return errors.New("manifest version not set")
+	}
+
+	if m.Header.Previous == 0 {
+		return errors.New("manifest previous not set")
+	}
+
+	if m.Header.FileCount == 0 {
+		return errors.New("manifest has a zero file count")
+	}
+
+	if m.Header.ContentSize == 0 {
+		return errors.New("manifest has zero contentsize")
+	}
+
+	if m.Header.TimeStamp.IsZero() {
+		return errors.New("manifest timestamp not set")
+	}
+
+	// Includes is not required
+	return nil
+}
+
+// ReadManifestFromFile reads a manifest file into memory
+func (m *Manifest) ReadManifestFromFile(f string) error {
+	var err error
+	manifestFile, err := os.Open(f)
+	if err != nil {
+		return err
+	}
+
+	// handle Close() errors
+	defer func() {
+		cerr := manifestFile.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+
+	fstat, err := manifestFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if fstat.Size() == 0 {
+		return fmt.Errorf("%v is an empty file", f)
+	}
+
+	input := bufio.NewScanner(manifestFile)
+
+	inHeader := true
+	for input.Scan() {
+		manifestLine := input.Text()
+		// empty line means end of header
+		if len(manifestLine) == 0 {
+			if inHeader {
+				inHeader = false
+				// reached end of header, validate that everything was set
+				if err = m.CheckHeaderPopulated(); err != nil {
+					return err
+				}
+				continue
+			} else {
+				// we already had a blank line, this is an error
+				return errors.New("extra blank line in manifest")
+			}
+		}
+
+		manifestFields := strings.Split(manifestLine, manifestFieldDelim)
+
+		// In the header until an empty line is encountered
+		if inHeader {
+			if err = readManifestFileHeaderLine(manifestFields, m); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// body if we got this far
+		if err = readManifestFileEntry(manifestFields, m); err != nil {
+			return err
+		}
+	}
+
+	if err = m.CheckHeaderPopulated(); err != nil {
+		return err
+	}
+
+	if len(m.Files) == 0 {
+		return errors.New("manifest does not have any file entries")
+	}
+
+	// return err so the deferred close can modify it
+	return err
+}
---
+++ src/swupd/manifest_test.go
@@ -0,0 +1,256 @@
+package swupd
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestReadManifestHeaderManifest(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"MANIFEST", "2"}, &m); err != nil {
+		t.Error("failed to read MANIFEST header")
+	}
+
+	if m.Header.Format != 2 {
+		t.Errorf("manifest Format header set to %d when 2 was expected", m.Header.Format)
+	}
+}
+
+func TestReadManifestHeaderManifestBad(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"MANIFEST", "i"}, &m); err == nil {
+		t.Error("readManifestFileHeaderLine did not fail with invalid format header")
+	}
+
+	if m.Header.Format != 0 {
+		t.Errorf("manifest Format header set to %d on invalid format", m.Header.Format)
+	}
+}
+
+func TestReadManifestHeaderVersion(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"version:", "10"}, &m); err != nil {
+		t.Error("failed to read version header")
+	}
+
+	if m.Header.Version != 10 {
+		t.Errorf("manifest Version header set to %d when 20 was expected", m.Header.Version)
+	}
+}
+
+func TestReadManifestHeaderVersionBad(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"version:", " "}, &m); err == nil {
+		t.Error("readManifestFileHeaderLine did not fail with invalid version header")
+	}
+
+	if m.Header.Version != 0 {
+		t.Errorf("manifest Version header set to %d on invalid version", m.Header.Version)
+	}
+}
+
+func TestReadManifestHeaderFilecount(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"filecount:", "1000"}, &m); err != nil {
+		t.Error("failed to read filecount header")
+	}
+
+	if m.Header.FileCount != 1000 {
+		t.Errorf("manifest FileCount header set to %d when 1000 was expected", m.Header.FileCount)
+	}
+}
+
+func TestReadManifestHeaderFilecountBad(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"filecount:", "i"}, &m); err == nil {
+		t.Error("readManifestFileHeaderLine did not fail with invalid filecount header")
+	}
+
+	if m.Header.FileCount != 0 {
+		t.Errorf("manifest FileCount header set to %d on invalid filecount", m.Header.FileCount)
+	}
+}
+
+func TestReadManifestHeaderTimestamp(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"timestamp:", "1000"}, &m); err != nil {
+		t.Error("failed to read timestamp header")
+	}
+
+	if m.Header.TimeStamp != time.Unix(1000, 0) {
+		t.Errorf("manifest TimeStamp header set to %v when 1000 was expected", m.Header.TimeStamp)
+	}
+}
+
+func TestReadManifestHeaderTimestampBad(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"timestamp:", "i"}, &m); err == nil {
+		t.Error("readManifestFileHeaderLine did not fail with invalid timestamp header")
+	}
+
+	if !m.Header.TimeStamp.IsZero() {
+		t.Errorf("manifest TimeStamp header set to %v on invalid timestamp", m.Header.TimeStamp)
+	}
+}
+
+func TestReadManifestHeaderContentsize(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"contentsize:", "1000"}, &m); err != nil {
+		t.Error("failed to read contentsize header")
+	}
+
+	if m.Header.ContentSize != 1000 {
+		t.Errorf("manifest ContentSize header set to %d when 1000 was expected", m.Header.ContentSize)
+	}
+}
+
+func TestReadManifestHeaderContentsizeBad(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"contentsize:", "i"}, &m); err == nil {
+		t.Error("readManifestFileHeaderLine did not fail with invalid contentsize header")
+	}
+
+	if m.Header.ContentSize != 0 {
+		t.Errorf("manifest ContentSize header set to %d on invalid contentsize", m.Header.ContentSize)
+	}
+}
+
+func TestReadManifestHeaderIncludes(t *testing.T) {
+	m := Manifest{}
+	if err := readManifestFileHeaderLine([]string{"includes:", "test-bundle"}, &m); err != nil {
+		t.Error("failed to read includes header")
+	}
+
+	var expected []*Manifest
+	expected = append(expected, &Manifest{Name: "test-bundle"})
+	if !reflect.DeepEqual(m.Header.Includes, expected) {
+		t.Errorf("manifest Includes set to %v when %v expected", m.Header.Includes, expected)
+	}
+
+	if err := readManifestFileHeaderLine([]string{"includes:", "test-bundle2"}, &m); err != nil {
+		t.Error("failed to read second includes header")
+	}
+
+	expected = append(expected, &Manifest{Name: "test-bundle2"})
+	if !reflect.DeepEqual(m.Header.Includes, expected) {
+		t.Errorf("manifest Includes set to %v when %v expected", m.Header.Includes, expected)
+	}
+}
+
+func TestReadManifestFileEntry(t *testing.T) {
+	validHash := "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+	validManifestLines := [][]string{
+		{"Fdbr", validHash, "10", "/usr/testfile"},
+		{"FgCr", validHash, "100", "/usr/bin/test"},
+		{"Ddsr", validHash, "99990", "/"},
+	}
+
+	t.Run("valid", func(t *testing.T) {
+		m := Manifest{}
+		for _, line := range validManifestLines {
+			if err := readManifestFileEntry(line, &m); err != nil {
+				t.Errorf("failed to read manifest line: %v", err)
+			}
+		}
+
+		for _, f := range m.Files {
+			if f.Type == 0 || f.Status == 0 || f.Modifier == 0 || !f.Rename {
+				t.Error("failed to set flag from manifest line")
+			}
+		}
+	})
+
+	invalidHash := "1234567890abcdef1234567890"
+	invalidManifestLines := [][]string{
+		{"..i.", validHash, "10", "/usr/testfile"},
+		{"...", validHash, "10", "/usr/testfile"},
+		{"FgCr", invalidHash, "100", "/usr/bin/test"},
+		{"Ddsr", validHash, "i", "/"},
+	}
+
+	for _, line := range invalidManifestLines {
+		t.Run("valid", func(t *testing.T) {
+			m := Manifest{}
+			if err := readManifestFileEntry(line, &m); err == nil {
+				t.Error("readManifestFileEntry did not fail with invalid input")
+			}
+		})
+	}
+}
+
+func TestCheckHeaderPopulated(t *testing.T) {
+	t.Run("populated", func(t *testing.T) {
+		m := Manifest{
+			Header: ManifestHeader{
+				Format:      10,
+				Version:     100,
+				Previous:    90,
+				FileCount:   553,
+				ContentSize: 100000,
+				TimeStamp:   time.Unix(1000, 0),
+				// does not fail when includes not added
+			},
+		}
+
+		if err := m.CheckHeaderPopulated(); err != nil {
+			t.Error("CheckHeaderPopulated raised error for valid header")
+		}
+	})
+
+	zeroTime := time.Time{}
+	var includes []*Manifest
+	invalidHeaders := []ManifestHeader{
+		{0, 100, 90, 553, time.Unix(1000, 0), 100000, includes},
+		{10, 0, 90, 553, time.Unix(1000, 0), 100000, includes},
+		{10, 100, 0, 553, time.Unix(1000, 0), 100000, includes},
+		{10, 100, 90, 0, time.Unix(1000, 0), 100000, includes},
+		{10, 100, 90, 553, zeroTime, 100000, includes},
+		{10, 100, 90, 553, time.Unix(1000, 0), 0, includes},
+	}
+
+	for _, header := range invalidHeaders {
+		t.Run("unpopulated", func(t *testing.T) {
+			m := Manifest{Header: header}
+			if err := m.CheckHeaderPopulated(); err == nil {
+				t.Error("CheckHeaderPopulated did not return an error on invalid header")
+			}
+		})
+	}
+}
+
+func TestReadManifestFromFileGood(t *testing.T) {
+	path := "testdata/manifest.good"
+	var m Manifest
+	if err := m.ReadManifestFromFile(path); err != nil {
+		t.Error(err)
+	}
+
+	if len(m.Files) == 0 {
+		t.Error("ReadManifestFromFile did not add file entries to the file list")
+	}
+}
+
+func TestReadManifestFromFileMissingFileCount(t *testing.T) {
+	path := "testdata/manifest.missingFilecount"
+	var m Manifest
+	if err := m.ReadManifestFromFile(path); err == nil {
+		t.Error("ReadManifestFromFile did not raise error on missing header")
+	}
+}
+
+func TestReadManifestFromFileMissingFiles(t *testing.T) {
+	path := "testdata/manifest.missingFiles"
+	var m Manifest
+	if err := m.ReadManifestFromFile(path); err == nil {
+		t.Error("ReadManifestFromFile did not raise error when missing file entries")
+	}
+}
+
+func TestReadManifestFromFileEmpty(t *testing.T) {
+	path := "testdata/manifest.empty"
+	var m Manifest
+	if err := m.ReadManifestFromFile(path); err == nil {
+		t.Error("ReadManifestFromFile did not raise error on empty file")
+	}
+}

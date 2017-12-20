da03c913ee4e54516e12dcdcc876c782f096018b
Tudor Marcu <tudor.marcu@intel.com>
2017-12-19 16:22:29 -0800

    Mixer: Update CLI to use cobra package
    The cobra package is very mature for writing CLIs, and makes the code
    much easier to maintain and add new commands to. This feature ports the
    current implementation over to using cobra, maintaining the same calling
    convention for the commands. However, it would be trivial and perhaps
    better to update the form from:
    mixer build-chroots --flags
    
    to
    
    mixer build chroots --flags
    
    with chroots being a subcommand of build, as the code is currently
    structured in cmd.
    
    Signed-off-by: Tudor Marcu <tudor.marcu@intel.com>

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,3 +1,17 @@
+// Copyright © 2017 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package helpers
 
 import (
@@ -233,5 +247,5 @@ func CheckRPM(rpm string) error {
 	if strings.Contains(string(output), "RPM v") {
 		return nil
 	}
-	return fmt.Errorf("ERROR: %s is not valid!", rpm)
+	return fmt.Errorf("ERROR: %s is not valid", rpm)
 }

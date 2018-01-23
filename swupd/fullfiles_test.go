a2cbf3b399a91df9102d4a7b77eb2726561b8b27
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2018-01-23 13:55:34 -0800

    swupd: add more cases to TestCreateFullfiles
    Add test cases for directory and symlink.
    
    Removed the check for mustNotExist, as there may be the case the same
    hash is requested in the current version as in a previous
    version. This happens for example in case of directories with same
    permissions from different versions (case "G" and "H").
    
    Signed-off-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

--- swupd/fullfiles_test.go
+++ swupd/fullfiles_test.go
@@ -24,19 +24,34 @@ func TestCreateFullfiles(t *testing.T) {
 	mustMkdir(t, outputDir)
 
 	files := map[string]*struct {
-		data    []byte
-		version uint32
-		hash    string
+		Contents string
+		Version  uint32
+		Mode     os.FileMode
+		IsDir    bool
+		Linkname string
+
+		ExpectedHash string
 	}{
-		"A": {data: []byte(`file1`), version: 20},
-		"B": {data: []byte(`file2`), version: 20},
-		"C": {data: []byte(`file3`), version: 20},
+		"A": {Contents: `file1`, Version: 20, Mode: 0755},
+		"B": {Contents: `file2`, Version: 20, Mode: 0644},
+		"C": {Contents: `file3`, Version: 20, Mode: 0644},
 
 		// File from previous version will not have a fullfile.
-		"D": {data: []byte(`DDD`), version: 10},
+		"D": {Contents: `DDD`, Version: 10, Mode: 0755},
 
-		// File with same content (and hash).
-		"E": {data: []byte(`file1`), version: 20},
+		// File with same content and mode (and hash).
+		"E": {Contents: `file1`, Version: 20, Mode: 0755},
+
+		// File with same content but different mode.
+		"F": {Contents: `file1`, Version: 20, Mode: 0644},
+
+		// Directories.
+		"G": {IsDir: true, Version: 20},
+		"H": {IsDir: true, Version: 10},
+
+		// Links.
+		"I": {Linkname: "A", Version: 20},
+		"J": {Linkname: "A", Version: 10},
 	}
 
 	m := &Manifest{}
@@ -44,22 +59,47 @@ func TestCreateFullfiles(t *testing.T) {
 
 	unique := make(map[Hashval]bool)
 	for name, desc := range files {
+		var typeFlag TypeFlag
 		path := filepath.Join(chrootDir, name)
-		err = ioutil.WriteFile(path, desc.data, 0644)
+		switch {
+		case desc.IsDir:
+			typeFlag = TypeDirectory
+			if desc.Mode == 0 {
+				desc.Mode = 0755
+			}
+			err = os.Mkdir(path, desc.Mode)
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = os.Chmod(path, desc.Mode)
+		case desc.Linkname != "":
+			typeFlag = TypeLink
+			err = os.Symlink(desc.Linkname, path)
+		default:
+			typeFlag = TypeFile
+			if desc.Mode == 0 {
+				desc.Mode = 0644
+			}
+			err = ioutil.WriteFile(path, []byte(desc.Contents), desc.Mode)
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = os.Chmod(path, desc.Mode)
+		}
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		desc.hash, err = GetHashForFile(path)
+		desc.ExpectedHash, err = GetHashForFile(path)
 		if err != nil {
 			t.Fatalf("couldn't get hashes for the test file %s: %s", path, err)
 		}
 
 		f := &File{
 			Name:    name,
-			Hash:    internHash(desc.hash),
-			Type:    TypeFile,
-			Version: desc.version,
+			Hash:    internHash(desc.ExpectedHash),
+			Type:    typeFlag,
+			Version: desc.Version,
 		}
 		if m.Header.Version == f.Version {
 			unique[f.Hash] = true
@@ -74,12 +114,10 @@ func TestCreateFullfiles(t *testing.T) {
 
 	// All correct files were created.
 	for _, desc := range files {
-		tarName := filepath.Join(outputDir, desc.hash+".tar")
-		if desc.version != m.Header.Version {
-			mustNotExist(t, tarName)
+		if desc.Version != m.Header.Version {
 			continue
 		}
-
+		tarName := filepath.Join(outputDir, desc.ExpectedHash+".tar")
 		mustExist(t, tarName)
 		mustHaveMatchingHash(t, tarName)
 	}

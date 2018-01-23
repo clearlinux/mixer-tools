307f51784e29ba131141d03a695f8d589bf0c874
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2018-01-23 13:38:10 -0800

    swupd: check for hashes in TestCreateFullfiles
    Use real hashes in the tests, and verify that the contents generated
    is correct (by verifying its hash).

--- swupd/fullfiles_test.go
+++ swupd/fullfiles_test.go
@@ -1,8 +1,12 @@
 package swupd
 
 import (
+	"archive/tar"
+	"io"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"syscall"
 	"testing"
 )
 
@@ -40,13 +44,16 @@ func TestCreateFullfiles(t *testing.T) {
 
 	unique := make(map[Hashval]bool)
 	for name, desc := range files {
-		err = ioutil.WriteFile(filepath.Join(chrootDir, name), desc.data, 0644)
+		path := filepath.Join(chrootDir, name)
+		err = ioutil.WriteFile(path, desc.data, 0644)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		// TODO: Use the proper hash function to derive this, then check the output later.
-		desc.hash = "hash" + string(desc.data)
+		desc.hash, err = GetHashForFile(path)
+		if err != nil {
+			t.Fatalf("couldn't get hashes for the test file %s: %s", path, err)
+		}
 
 		f := &File{
 			Name:    name,
@@ -65,23 +72,85 @@ func TestCreateFullfiles(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// All correct files were created.
 	for _, desc := range files {
 		tarName := filepath.Join(outputDir, desc.hash+".tar")
 		if desc.version != m.Header.Version {
 			mustNotExist(t, tarName)
-		} else {
-			mustExist(t, tarName)
+			continue
 		}
+
+		mustExist(t, tarName)
+		mustHaveMatchingHash(t, tarName)
 	}
 
+	// No extra files were created.
 	fis, err := ioutil.ReadDir(outputDir)
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	if len(fis) != len(unique) {
 		t.Fatalf("generated %d fullfiles, but want %d", len(fis), len(unique))
 	}
+}
 
-	// TODO: Extract the tar files and retake the hash to see if it matches.
+func mustHaveMatchingHash(t *testing.T, path string) {
+	expectedHash := filepath.Base(path)
+	// Take the ".tar" extension off.
+	expectedHash = expectedHash[:len(expectedHash)-4]
+
+	f, err := os.Open(path)
+	if err != nil {
+		t.Fatalf("couldn't open %s to check contents hash: %s", path, err)
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+
+	tr, err := newCompressedTarReader(f)
+	if err != nil {
+		t.Fatalf("couldn't uncompress %s to check contents hash: %s", path, err)
+	}
+	defer func() {
+		_ = tr.Close()
+	}()
+
+	hdr, err := tr.Next()
+	if err != nil {
+		t.Fatalf("couldn't read archive in %s: %s", path, err)
+	}
+
+	h, err := newHashFromTarHeader(hdr)
+	if err != nil {
+		t.Fatalf("couldn't create hash struct from %s: %s", path, err)
+	}
+
+	_, err = io.Copy(h, tr)
+	if err != nil {
+		t.Fatalf("couldn't read archive %s contents: %s", path, err)
+	}
+
+	hash := h.Sum()
+	if hash != expectedHash {
+		t.Fatalf("unexpected hash %s for contents of %s", hash, path)
+	}
+}
+
+func newHashFromTarHeader(hdr *tar.Header) (*Hash, error) {
+	info := &HashFileInfo{
+		Mode:     uint32(hdr.Mode),
+		UID:      uint32(hdr.Uid),
+		GID:      uint32(hdr.Gid),
+		Size:     hdr.Size,
+		Linkname: hdr.Linkname,
+	}
+	switch hdr.Typeflag {
+	case tar.TypeReg, tar.TypeRegA:
+		info.Mode |= syscall.S_IFREG
+	case tar.TypeDir:
+		info.Mode |= syscall.S_IFDIR
+	case tar.TypeSymlink:
+		info.Mode |= syscall.S_IFLNK
+	}
+	return NewHash(info)
 }

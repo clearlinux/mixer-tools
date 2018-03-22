7753791d8d4830104815c941ca670015651efa22
Kevin C. Wells <kevin.c.wells@intel.com>
2018-03-22 18:05:34 +0000

    Release v4.2.0
    This release contains four major additions:
    
    1) Support for package-level bundles. A new local and upstream package
    list has been introduced, which mixer parses to automatically generate
    single-package bundles.
    
    2) The bundle building algorithm has been significantly optimized. This
    step now runs approximately three times faster than the previous version.
    
    3) New TOML configuration support. Mixer now has an optional
    '--new-config' flag that adds support for TOML-based configuration. A
    new 'mixer config' command has been added to validate existing
    builder.conf files, as well as convert legacy (INI) files into the new
    TOML format.
    
    4) Update to how mixer generates the DNF conf file used during bundle
    building. This removes the dependency on m4.
    
    Signed-off-by: Kevin C. Wells <kevin.c.wells@intel.com>

--- builder/builder.go
+++ builder/builder.go
@@ -41,7 +41,7 @@ import (
 )
 
 // Version of Mixer. Also used by the Makefile for releases.
-const Version = "4.1.2"
+const Version = "4.2.0"
 
 // UseNewSwupdServer controls whether to use the new implementation of
 // swupd-server (package swupd) when possible. This is an experimental feature.

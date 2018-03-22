6711f2f60856b29e9ddd90495ab79b00efc49991
Reagan Lopez <reagan.lopez@intel.com>
2018-03-22 20:10:37 +0000

    dm-verity on rootfs data partition using systemd-veritysetup-generator
    - Used kernel cmdline root=/dev/mapper/root
    - Unsuccessful.
    - Kernel panic - not syncing: VFS: Unable to mount root fs on unknown-block(0,0)

--- verified-boot/dm-verity-impl-1.py
+++ verified-boot/dm-verity-impl-1.py
@@ -23,15 +23,9 @@ hash_num = 4
 boot_dev = dev[0] + "p" + str(boot_num)
 data_dev = dev[0] + "p" + str(data_num)
 hash_dev = dev[0] + "p" + str(hash_num)
-
-verity_name = "vroot1"
-initramfs_gen = "initramfs.sh"
-boot_gen = "boot.sh"
-initramfs_fname = "custom_initramfs.cpio.gz"
-
+verity_name = "root"
 subprocess.check_output("rm -rf mnt".split(" "))
 subprocess.check_output("mkdir mnt".split(" "))
-
 
 cmd = "veritysetup --verbose --data-block-size=1024 --hash-block-size=1024 format {0} {1}".format(data_dev, hash_dev)
 print("Executing: " + cmd)
@@ -39,9 +33,7 @@ try:
     res = subprocess.check_output(cmd.split(" ")).decode("utf-8").splitlines()
 except Exception:
     raise Exception("{0}: {1}".format(cmd, sys.exc_info()))
-
 print(res[len(res) - 1])
-
 salt_str = res[len(res) - 3]
 root_hash_str = res[len(res) - 2]
 print(salt_str)
@@ -50,34 +42,6 @@ salt = salt_str.replace('Salt:            	','')
 root_hash = root_hash_str.replace('Root hash:      	','')
 print(salt)
 print(root_hash)
-
-print("Creating initramfs..")
-subprocess.check_output("sh {0}".format(initramfs_gen).split(" "))
-
-print("Generating the init file..")
-subprocess.check_output("touch initramfs/init".split(" "))
-try:
-    outfile = open('initramfs/init', 'w')
-    outfile.write("#!/bin/sh\n")
-    outfile.write("/bin/echo \"Inside initramfs init...\"\n")
-    outfile.write("/bin/mount -t proc none /proc\n")
-    outfile.write("/bin/mount -t sysfs none /sys\n")
-    outfile.write("/bin/mount -t devtmpfs none /dev\n")
-
-    outfile.write("/bin/echo \"Executing veritysetup...\"\n")
-    outfile.write("/bin/veritysetup --verbose --data-block-size=1024 --hash-block-size=1024 create " + verity_name + " /dev/sda" + str(data_num) + " /dev/sda" + str(hash_num) + " " + root_hash + "\n")
-    outfile.write("wait\n")
-    outfile.write("/bin/mount /dev/mapper/" + verity_name + " /mnt/root\n")
-    outfile.write("/bin/mount --move /proc /mnt/root/proc\n")
-    outfile.write("/bin/mount --move /sys /mnt/root/sys\n")
-    outfile.write("/bin/mount --move /dev /mnt/root/dev\n")
-    outfile.write("/bin/echo \"Executing switch_root...\"\n")
-
-    outfile.write("exec switch_root /mnt/root /sbin/init")
-    outfile.close()
-except IOError:
-    print("I/O error")
-#subprocess.check_output("chmod +x initramfs/init".split(" "))
 
 subprocess.check_output("mount {0} mnt".format(boot_dev).split(" "))
 for fname in os.listdir('mnt/loader/entries/'):
@@ -90,47 +54,33 @@ for fname in os.listdir('mnt/loader/entries/'):
             outfile.close()
         except IOError:
             print("I/O error")
-        content = re.sub(r"root=.* quiet", "rootdelay=10 quiet", content)
-        content = re.sub(r"init=.* initcall_debug", "", content)
-        content = re.sub(r"rw", "", content)
+#        content = re.sub(r"root=PARTUUID=", "cryptdevice=UUID=", content)
+#        kernel_cmdline = ":" + verity_name + " quiet"
+#        content = re.sub(r" quiet", kernel_cmdline, content)
+        kernel_cmdline = "root=/dev/mapper/" + str(verity_name) + " systemd.verity=yes roothash=" + root_hash + " systemd.verity_root_data=/dev/sda" + str(data_num) + " systemd.verity_root_hash=/dev/sda" + str(hash_num) + " rootdelay=10 quiet"
+        content = re.sub(r"root=.* quiet", kernel_cmdline, content)
+        content = re.sub(r"rw", "ro", content)
         print(content)
 
         try:
             outfile = open(path, 'w')
             outfile.write(content)
-            outfile.write("initrd EFI/" + initramfs_fname)
             outfile.close()
         except IOError:
             print("I/O error")
 subprocess.check_output("umount mnt".split(" "))
 
-print("Updating boot files..")
-subprocess.check_output("sh {0} {1} {2}".format(boot_gen, boot_dev, initramfs_fname).split(" "))
-#subprocess.check_output("mount {0} mnt".format(boot_dev).split(" "))
-##subprocess.check_output("cd initramfs".split(" "))
-#os.chdir("initramfs")
-#print("Inside dir " + os.getcwd())
-#subprocess.check_output("find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../mnt/EFI/{0}".format(initramfs_fname).split(" "))
-##subprocess.check_output("cd ..".split(" "))
-#os.chdir("../")
-#print("Inside dir " + os.getcwd())
-#for fname in os.listdir('mnt/loader/entries/'):
-#    if (fnmatch.fnmatch(fname, 'Clear-*')):
-#        path = "mnt/loader/entries/" + fname
-#        print("Writing initramfs config to " + path + " in " + boot_dev)
-#        try:
-#            outfile = open(path, 'a')
-#            outfile.write("initrd EFI/" + initramfs_fname)
-#            outfile.close()
-#        except IOError:
-#            print("I/O error")
-#
-#
-#subprocess.check_output("umount mnt".split(" "))
-#subprocess.check_output("rm -rf mnt".split(" "))
-#
-#cmd = "veritysetup --verbose verify {0} {1} {2}".format(data_dev, hash_dev, root_hash)
-#
+subprocess.check_output("rm -rf mnt".split(" "))
+
+cmd = "veritysetup --verbose verify {0} {1} {2}".format(data_dev, hash_dev, root_hash)
+print("Executing: " + cmd)
+try:
+   res = subprocess.check_output(cmd.split(" ")).decode("utf-8").splitlines()
+except Exception:
+   raise Exception("{0}: {1}".format(cmd, sys.exc_info()))
+print(res[len(res) - 1])
+
+#cmd = "veritysetup --verbose --data-block-size=1024 --hash-block-size=1024 create " + verity_name + " {0} {1} {2}".format(data_dev, hash_dev, root_hash)
 #print("Executing: " + cmd)
 #try:
 #   res = subprocess.check_output(cmd.split(" ")).decode("utf-8").splitlines()

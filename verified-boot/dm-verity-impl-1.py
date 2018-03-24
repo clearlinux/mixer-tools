8c63b11e5f9908851358ad16fb75e8d4b7978943
Reagan Lopez <reagan.lopez@intel.com>
2018-03-24 00:00:51 +0000

    dm-verity on rootfs data partition using systemd-veritysetup-generator
    - Used kernel cmdline cryptdevice=UUID=*:root root=/dev/mapper/root with correct UUID.
    - Unsuccessful.
    - Kernel panic - not syncing: VFS: Unable to mount root fs on unknown-block(0,0)

--- verified-boot/dm-verity-impl-1.py
+++ verified-boot/dm-verity-impl-1.py
@@ -17,38 +17,28 @@ except Exception:
     raise Exception("{0}: {1}".format(cmd, sys.exc_info()))
 print(dev[len(dev) - 1])
 boot_num = 1
-rootfs_num = 3
-data_num = 4
-hash_num = 5
+data_num = 3
+hash_num = 4
 
 boot_dev = dev[0] + "p" + str(boot_num)
-rootfs_dev = dev[0] + "p" + str(rootfs_num)
 data_dev = dev[0] + "p" + str(data_num)
 hash_dev = dev[0] + "p" + str(hash_num)
 verity_name = "root"
-
-print("Creating data files in " + data_dev)
 subprocess.check_output("rm -rf mnt".split(" "))
 subprocess.check_output("mkdir mnt".split(" "))
-subprocess.check_output("mount {0} mnt".format(data_dev).split(" "))
 
-subprocess.check_output("touch mnt/file1.sh".split(" "))
+cmd = "blkid {0}".format(data_dev)
+print("Executing: " + cmd)
 try:
-    outfile = open('mnt/file1.sh','w')
-    outfile.write("echo Testing dm-verity data1...")
-    outfile.close()
-except IOError:
-    print("I/O error")
-
-subprocess.check_output("touch mnt/file2.sh".split(" "))
-try:
-    outfile = open('mnt/file2.sh','w')
-    outfile.write("echo Testing dm-verity data2...")
-    outfile.close()
-except IOError:
-    print("I/O error")
-
-subprocess.check_output("umount mnt".split(" "))
+    res = subprocess.check_output(cmd.split(" ")).decode("utf-8").splitlines()
+except Exception:
+    raise Exception("{0}: {1}".format(cmd, sys.exc_info()))
+s1 = res[0]
+print(s1)
+s2 = " UUID="
+s3 = s1[s1.index(s2) + len(s2):]
+data_dev_uuid = s3.split("\"", 2)[1]
+print("data_dev_uuid = " + data_dev_uuid)
 
 cmd = "veritysetup --verbose --data-block-size=1024 --hash-block-size=1024 format {0} {1}".format(data_dev, hash_dev)
 print("Executing: " + cmd)
@@ -56,9 +46,7 @@ try:
     res = subprocess.check_output(cmd.split(" ")).decode("utf-8").splitlines()
 except Exception:
     raise Exception("{0}: {1}".format(cmd, sys.exc_info()))
-
 print(res[len(res) - 1])
-
 salt_str = res[len(res) - 3]
 root_hash_str = res[len(res) - 2]
 print(salt_str)
@@ -68,10 +56,7 @@ root_hash = root_hash_str.replace('Root hash:      	','')
 print(salt)
 print(root_hash)
 
-
-kernel_cmdline = "quiet systemd.verity=yes roothash=" + root_hash + " systemd.verity_root_data=/dev/sda" + str(data_num) + " systemd.verity_root_hash=/dev/sda" + str(hash_num)
 subprocess.check_output("mount {0} mnt".format(boot_dev).split(" "))
-
 for fname in os.listdir('mnt/loader/entries/'):
     if (fnmatch.fnmatch(fname, 'Clear-*')):
         path = "mnt/loader/entries/" + fname
@@ -82,8 +67,12 @@ for fname in os.listdir('mnt/loader/entries/'):
             outfile.close()
         except IOError:
             print("I/O error")
-
-        content = re.sub(r"quiet", kernel_cmdline, content)
+#        content = re.sub(r"root=PARTUUID=", "cryptdevice=UUID=", content)
+#        kernel_cmdline = ":" + verity_name + " quiet"
+#        content = re.sub(r" quiet", kernel_cmdline, content)
+        kernel_cmdline = "cryptdevice=UUID=" + data_dev_uuid + ":" + str(verity_name) + " root=/dev/mapper/" + str(verity_name) + " systemd.verity=yes roothash=" + root_hash + " systemd.verity_root_data=/dev/sda" + str(data_num) + " systemd.verity_root_hash=/dev/sda" + str(hash_num) + " rootdelay=10 quiet"
+        content = re.sub(r"root=.* quiet", kernel_cmdline, content)
+        content = re.sub(r"rw", "ro", content)
         print(content)
 
         try:
@@ -92,12 +81,11 @@ for fname in os.listdir('mnt/loader/entries/'):
             outfile.close()
         except IOError:
             print("I/O error")
-
 subprocess.check_output("umount mnt".split(" "))
+
 subprocess.check_output("rm -rf mnt".split(" "))
 
 cmd = "veritysetup --verbose verify {0} {1} {2}".format(data_dev, hash_dev, root_hash)
-
 print("Executing: " + cmd)
 try:
    res = subprocess.check_output(cmd.split(" ")).decode("utf-8").splitlines()

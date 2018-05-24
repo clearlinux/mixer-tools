e0d0107e7514b6f4924839f947f020bd520db835
Tudor Marcu <tudor.marcu@intel.com>
2018-05-24 06:23:25 +0000

    Release v4.5.3
    This release fixes the check for changed includes, and fixes 'mixin' to
    support local content properly.
    
    Signed-off-by: Tudor Marcu <tudor.marcu@intel.com>

--- builder/builder.go
+++ builder/builder.go
@@ -42,7 +42,7 @@ import (
 )
 
 // Version of Mixer. Also used by the Makefile for releases.
-const Version = "4.5.2"
+const Version = "4.5.3"
 
 // Offline controls whether mixer attempts to automatically cache upstream
 // bundles. In offline mode, all necessary bundles must exist in local-bundles.
